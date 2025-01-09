53b5b356ed36016f809465cbec2bc1ca5037b175
Maltseva.Evgenia <evmaltseva74@gmail.com>
2025-01-09 21:26:01 +0100

    added new data

--- 26-08-24-wde.sh
+++ 26-08-24-wde.sh
@@ -8,3 +8,8 @@ if [ ! -d "$source_directory" ]; then
     echo "Директория '$source_directory' не существует или недоступна."
     exit 1
 fi
+# Проверка существования целевой директории
+if [ ! -d "$target_directory" ]; then
+    echo "Директория '$target_directory' не существует или недоступна."
+    exit 1
+fi
