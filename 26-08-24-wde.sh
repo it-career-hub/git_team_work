3c58321758c96ba32b592a77225ff22394d1bc12
NatBluh <nat.bluhme@gmail.com>
2025-01-09 21:11:45 +0100

    #2

--- 26-08-24-wde.sh
+++ 26-08-24-wde.sh
@@ -2,3 +2,9 @@
 
 read -p "Введите исходную директорию: " source_directory
 read -p "Введите целевую директорию: " target_directory
+
+# Проверка существования исходной директории
+if [ ! -d "$source_directory" ]; then
+    echo "Директория '$source_directory' не существует или недоступна."
+    exit 1
+fi
