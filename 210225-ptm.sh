afca2fc33d5f62f82cd972f5d2ae75fc28c91235
irynaPL99 <iryna.platonova99@gmail.com>
2025-05-26 12:02:28 +0200

    issue 4

--- 210225-ptm.sh
+++ 210225-ptm.sh
@@ -18,3 +18,12 @@ if [ ! -d "$target_directory" ]; then
    echo "Директория '$target_directory' не существует или недоступна."
    exit 1
 fi
+
+# Проверка, есть ли файлы с указанным расширением в исходной директории
+matching_files=$(find "$source_directory" -maxdepth 1 -type f -name "*.$file_extension")
+if [ -z "$matching_files" ]; then
+   echo "В директории '$source_directory' нет файлов с расширением '.$file_extension'."
+   exit 1
+fi
+
+
