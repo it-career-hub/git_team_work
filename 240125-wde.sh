b94c48e8047c21999b477b72ae110679cf5017bb
Frellfrell <ella.gubareva.24@gmail.com>
2025-05-28 21:13:36 +0200

    issues 3

--- 240125-wde.sh
+++ 240125-wde.sh
@@ -4,6 +4,15 @@
 read -p "Введите исходную директорию: " source_directory
 read -p "Введите целевую директорию: " target_directory
 read -p "Введите расширение файла: " file_extension
-
+# Проверка существования исходной директории
+if [ ! -d "$source_directory" ]; then
+echo "Директория '$source_directory' не существует или недоступна."
+exit 1
+fi
+# Проверка существования целевой директории
+if [ ! -d "$target_directory" ]; then
+echo "Директория '$target_directory' не существует или недоступна."
+exit 1
+fi
 
 
