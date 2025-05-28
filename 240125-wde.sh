7a1aa06ae5c2fa2a779f621ed9352357a627d3b7
PavelLiuts <lutskanov1981@gmail.com>
2025-05-28 21:21:25 +0200

    issue5

--- 240125-wde.sh
+++ 240125-wde.sh
@@ -4,15 +4,23 @@
 read -p "Введите исходную директорию: " source_directory
 read -p "Введите целевую директорию: " target_directory
 read -p "Введите расширение файла: " file_extension
+
 # Проверка существования исходной директории
 if [ ! -d "$source_directory" ]; then
 echo "Директория '$source_directory' не существует или недоступна."
 exit 1
 fi
+
 # Проверка существования целевой директории
 if [ ! -d "$target_directory" ]; then
 echo "Директория '$target_directory' не существует или недоступна."
 exit 1
 fi
 
+# Копирование файлов с указанным расширением в целевую директорию
+for file in $matching_files; do
+cp "$file" "$target_directory"
+echo "Скопирован файл: $file"
+done
+echo "Готово."
 
