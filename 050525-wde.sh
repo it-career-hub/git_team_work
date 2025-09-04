1316bf5ee4881dee6b2663244645d03bb0b1d2c8
iohichu <63808665+iohichu@users.noreply.github.com>
2025-09-04 21:37:16 +0200

    Update 050525-wde.sh

--- 050525-wde.sh
+++ 050525-wde.sh
@@ -17,3 +17,18 @@ if [ ! -d "$target_directory" ]; then
 echo "Директория '$target_directory' не существует или недоступна."
 exit 1
 fi
+# Проверка, есть ли файлы с указанным расширением в исходной директории
+matching_files=$(find "$source_directory" -maxdepth 1 -type f -name
+"*.$file_extension")
+if [ -z "$matching_files" ]; then
+
+echo "В директории '$source_directory' нет файлов с расширением
+'.$file_extension'."
+exit 1
+fi
+# Копирование файлов с указанным расширением в целевую директорию
+for file in $matching_files; do
+cp "$file" "$target_directory"
+echo "Скопирован файл: $file"
+done
+echo "Готово."
