968ede746184a78575263c2ace2d568ad16306f4
AnnaLindemann <anshalneva@gmail.com>
2025-09-04 21:24:36 +0200

    Added file

--- 050525-wde.sh
+++ 050525-wde.sh
@@ -11,3 +11,9 @@ if [ ! -d "$source_directory" ]; then
 echo "Директория '$source_directory' не существует или недоступна."
 exit 1
 fi
+
+# Проверка существования целевой директории
+if [ ! -d "$target_directory" ]; then
+echo "Директория '$target_directory' не существует или недоступна."
+exit 1
+fi
