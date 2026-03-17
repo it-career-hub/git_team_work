2fcaf39a77b10d9f9f148f61c99deeda78661b63
Jazyhuligan <nazartarnavskyi1997@gmail.com>
2026-03-17 11:29:02 +0100

    completed issue3

--- 281125-wdm.sh
+++ 281125-wdm.sh
@@ -4,4 +4,8 @@ read -p "Введите целевую директорию: " target_directory
 
 
 read -p "Введите расширение файлов для копирования (без точки): " file_extension
- 
+
+if [ ! -d "$source_directory" ]; then
+echo "Директория '$source_directory' не существует или недоступна."
+exit 1
+fi
