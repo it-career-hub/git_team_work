cc09b258a0b5277b154956a634f7f61e1bd285fc
Ekaterina <e406096@gmail.com>
2026-03-17 13:14:44 +0300

    added issue2

--- 281125-wdm.sh
+++ 281125-wdm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 read -p "Введите исходную директорию: " source_directory
 read -p "Введите целевую директорию: " target_directory
+read -p "Введите расширение файлов для копирования (без точки): " file_extension  
