abc0177fb8569fe0b0f0e85aa7f933b2506dcdf6
Nadezhda Ozbey <nad.ozbey@gmail.com>
2026-03-17 11:36:25 +0100

    fixed

--- 281125-wdm.sh
+++ 281125-wdm.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+bin/bash
 read -p "Введите исходную директорию: " source_directory
 read -p "Введите целевую директорию: " target_directory
 
 
 read -p "Введите расширение файлов для копирования (без точки): " file_extension
- 
+if [ -d "$source_directory" ]; then 
+echo "Директория '$source_directory' не существует или недоступна."
+exit 1
+fi
+
+if [ -d "$target_directory" ]; then
+echo "Директория '$target_directory' не существует или недоступна."
+exit 1
+fi
